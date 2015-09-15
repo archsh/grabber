b088bde2dc859cefb287dde99499dc0a77b72dab
Mingcai SHEN <archsh@gmail.com>
2015-09-15 18:10:57 +0800

    Updated.

--- src/grabber/main.go
+++ src/grabber/main.go
@@ -15,11 +15,14 @@ func main() {
 	var api_id string
 	var api_key string
 	var config_filename string
+	var daemonize bool
 	flag.StringVar(&log_filename, "l", "", "Log filename. Default is empty, and log to stdout.")
 	flag.StringVar(&server_url, "u", "http://localhost:8938/", "Server URL for report interface.")
 	flag.StringVar(&api_id, "id", "", "API ID for server API access.")
 	flag.StringVar(&api_key, "key", "", "API Key for server API access.")
 	flag.StringVar(&config_filename, "c", "", "Configration file.")
+	flag.Bool("d", false, "Run in deamon mode.")
+	flag.Parse()
 
 	if api_id == "" || api_key == "" || server_url == "" {
 		flag.PrintDefaults()
